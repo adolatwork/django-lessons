978d358e0354b100b8035d6999faaab37c7b5c30
abdulazizkomilov <adolat.work@gmail.com>
2025-07-18 08:36:24 +0500

    model view template dars tayyor

--- newsProject/news/models.py
+++ newsProject/news/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
