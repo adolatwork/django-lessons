9f6ab5e5e665ac8fd87c253298cd7286b5bbd2a0
abdulazizkomilov <abdulazizkomilov2001@gmail.com>
2025-11-01 16:35:03 +0500

    Blog yaratish 1 qism

--- 04-BlogProject/blog/models.py
+++ 04-BlogProject/blog/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title =models.CharField(max_length=200)
     author = models.ForeignKey(
         'auth.User',
-         on_delets=models.CASCADE
+        on_delete=models.CASCADE
     )
     body = models.TextField()
-    
-    def __str__(self):   
+
+    def __str__(self):
         return self.title
